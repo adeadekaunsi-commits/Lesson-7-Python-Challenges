9e12733a943e5f24636d3597cff81cb1eb2b1411
adeadekaunsi-commits <ade.adekaunsi@gmail.com>
2026-05-12 13:26:51 +0100

    Activity 1,2,3 completed

---
+++ A3.py
@@ -0,0 +1,14 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+#sum of 40 numbers
+sum = mean1*total_number
+print("Sum of 40 numbers: " + str(sum))
+
+#correct sum of these numbers
+num2=sum-((wrong_number) - (correct_number))
+print("Correct sum of 40 numbers: " + str(num2))
+
+#the correct mean
+mean2=num2/total_number
